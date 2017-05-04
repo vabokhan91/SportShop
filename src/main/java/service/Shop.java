0b1a3faaf2008e5851d93db2c4d7b50218b73e8c
va.bokhan91 <va.bokhan91@gmail.com>
2017-05-04 15:21:17 +0300

    some changes

--- src/main/java/service/Shop.java
+++ src/main/java/service/Shop.java
@@ -11,13 +11,19 @@ import java.util.*;
  * Created by vbokh on 02.05.2017.
  */
 public class Shop {
-    /** Character used for splitting strings, while reading from file in initStore method*/
+    /**
+     * Character used for splitting strings, while reading from file in initStore method
+     */
     private static final String COMMA_DELIMITER = ",";
 
-    /** Used for keeping available sport equipment. Map-value - number of available equipment*/
+    /**
+     * Used for keeping available sport equipment. Map-value - number of available equipment
+     */
     private Map<SportEquipment, Integer> goods;
 
-    /** Used for keeping rented sport equipment. Map-value - number of equipment rented*/
+    /**
+     * Used for keeping rented sport equipment. Map-value - number of equipment rented
+     */
     private Map<SportEquipment, Integer> rentedGoods;
 
     public Shop(Map<SportEquipment, Integer> goods) {
@@ -39,15 +45,14 @@ public class Shop {
     }
 
     /**
-    * Rents sport equipment to clients. If equipment is available and customer can rent more equipment, then decreases
+     * Rents sport equipment to clients. If equipment is available and customer can rent more equipment, then decreases
      * number of equipment in map goods by 1, adds equipment to array of customers' rented goods and increases number of equipment by 1
      * in map of rented goods.
-    *
-    * @param customer the object which is checked, if it is allowed to rent more equipment.
+     *
+     * @param customer  the object which is checked, if it is allowed to rent more equipment.
      * @param equipment the object, which is checked, if there is available equipment in the shop. The object then stored in the map
      *                  of rented goods,
-     *
-    */
+     */
     public void rent(Customer customer, SportEquipment equipment) {
         if (isEquipmentAvailable(equipment)) {
             if (checkQuantity(customer)) {
@@ -61,16 +66,14 @@ public class Shop {
             } else {
                 System.out.println("Can not rent equipment. Customer " + customer.getId() + " " + customer.getName() + " has already borrowed > 3 units.");
             }
-
-
         } else {
             System.out.println("There is no equipment available");
         }
     }
 
     /**
-    *  Checks if requested equipment is available.
-    */
+     * Checks if requested equipment is available.
+     */
     private boolean isEquipmentAvailable(SportEquipment equipment) {
         if (goods.get(equipment) > 0) {
             return true;
@@ -78,8 +81,8 @@ public class Shop {
     }
 
     /**
-    * Checks the number of equipment, which customer has already rented.
-    */
+     * Checks the number of equipment, which customer has already rented.
+     */
     private boolean checkQuantity(Customer customer) {
         if (customer.getBorrowedGoods().getUnits().length > 3) {
             return false;
@@ -87,9 +90,9 @@ public class Shop {
     }
 
     /**
-    * Searches the requested equipment by title and uses overlap between the entered parameter (name) and the title field of sportequipment object.
-    * Returns object of type SportEquipment, if found, or null.
-    */
+     * Searches the requested equipment by title and uses overlap between the entered parameter (name) and the title field of sportequipment object.
+     * Returns object of type SportEquipment, if found, or null.
+     */
     public SportEquipment findByTitle(String name) {
         SportEquipment good = null;
         Set<SportEquipment> keyset = goods.keySet();
@@ -102,13 +105,12 @@ public class Shop {
                 break;
             }
         }
-
         return good;
     }
 
     /**
-    * Prints available equipment in the shop.
-    */
+     * Prints available equipment in the shop.
+     */
     public void printAvailiableEquipment() {
         Set<SportEquipment> available = goods.keySet();
         for (SportEquipment unit : available) {
@@ -117,8 +119,8 @@ public class Shop {
     }
 
     /**
-    * Prints rented equipment.
-    */
+     * Prints rented equipment.
+     */
     public void printRentedGoods() {
         Set<SportEquipment> rented = rentedGoods.keySet();
         for (SportEquipment unit : rented) {
@@ -127,10 +129,10 @@ public class Shop {
     }
 
     /**
-    * Initializes shop store from file.
-    */
+     * Initializes shop store from file.
+     */
     private Map<SportEquipment, Integer> initStore(String filename) {
-        Map<SportEquipment, Integer> shopequipment = new HashMap<SportEquipment, Integer>();
+        Map<SportEquipment, Integer> shopEquipment = new HashMap<SportEquipment, Integer>();
         File initializationFile = new File(filename);
         SportEquipment sportEquipment;
         BufferedReader reader = null;
@@ -148,21 +150,21 @@ public class Shop {
                 int price = Integer.parseInt(tokens[2]);
                 Integer number = Integer.parseInt(tokens[3]);
                 sportEquipment = new SportEquipment(category, title, price);
-                shopequipment.put(sportEquipment, number);
+                shopEquipment.put(sportEquipment, number);
             }
         } catch (FileNotFoundException e) {
             System.out.println(" No file named " + initializationFile.getName() + " was found.");
         } catch (IOException e) {
             System.out.println("Error while reading from file : " + filename);
-        }finally {
-            if(reader!=null){
-                try{
+        } finally {
+            if (reader != null) {
+                try {
                     reader.close();
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-        return shopequipment;
+        return shopEquipment;
     }
 }
